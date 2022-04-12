743875417f3f0b96a8be63a854485ca47a9b5441
Rachel E E <62678193+rachelish@users.noreply.github.com>
2022-04-12 14:49:03 +0000

    docstrings

--- StringUtility.py
+++ StringUtility.py
@@ -1,11 +1,14 @@
 class StringUtility:
   
   def __init__(self, string):
+    """initializes the variable string, takes string as an arguemnt"""
     self.string = string
     
   def __str__(self):
+    """ idk """
     return self.string
   def vowels(self):
+    """counts how many vowels in string, but returns many if its greater than 5 """
     vow = ["a", "e", "i", "o", "u"]
     count = 0
     for letter in self.string:
@@ -16,11 +19,13 @@ class StringUtility:
     else:
         return "many"
   def bothEnds(self):
+    """only returns first+last 2 characters in string if the length is greater than 2 characters"""
     if len(self.string) <= 2:
       return ""
     return self.string[:2] + self.string[-2:]
     
   def fixStart(self):  
+    """replaces any letter in string that matches the first letter with star. does not replace first letter. if string is not greater than 1 character, returns original string"""
     expected = ""
     if len(self.string) <= 1:
       return self.string
@@ -38,11 +43,13 @@ class StringUtility:
     #change expected to word
          
   def asciiSum(self):
+    """adds ascii sums of each character in string"""
     expected = 0
     for i in self.string:
       expected += int(ord(i))
     return expected
   def cipher(self):
+    """shifts each character in string by the number of characters in string. repeats alphabet if shifted outside of alphabetical ascii values"""
     expected ="" 
     for i in self.string:
       if i == ' ':
